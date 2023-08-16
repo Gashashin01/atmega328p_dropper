7a0475bda39498e8761d3e90415b4c9e8cbede3d
Emil <ferguspetr89@gmail.com>
2023-08-16 12:47:27 +0300

    define SERVO and F_CPU

--- main.c
+++ main.c
@@ -1,25 +1,29 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-void ADC_init(){
-	ADMUX|=(1<<REFS0); // Настройка опорного напряжения (AREF) на VCC
+#define SERVO PB1
 
-	ADCSRA|=(1<<ADEN); // Разрешить работу АЦП
-	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // делитель 128
+#define F_CPU 1000000UL
+
+void ADC_init(){
+	ADMUX|=(1<<REFS0); // Setting the voltage reference (AREF) to VCC
+
+	ADCSRA|=(1<<ADEN); // Enable ADC
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // divider 128
 
 }
 
 uint16_t ADC_read(uint8_t channel){
-	// Выбор аналогового канала (0-7)
+	// Select analog channel (0-7)
 	ADMUX = (ADMUX & 0xF0) | (channel & 0x0F);
 
-	// Запуск однократного преобразования
+	// Run a single conversion
 	ADCSRA |= (1 << ADSC);
 
-	// Ожидание завершения преобразования
+	// Waiting for the conversion to complete
 	while (ADCSRA & (1 << ADSC));
 
-	// Возврат результата
+	// Returning a result
 	return ADC;
 }
 
@@ -27,28 +31,24 @@ int main()
 {
 	ADC_init();
 
-	DDRB |= (1<<PB1);
-	PORTB = (1 << PB1);
+	DDRB |= (1<<SERVO);
+	PORTB = (1 << SERVO);
 	_delay_ms(1);
 	PORTB=0b00000000;
 	_delay_ms(500);
 
 	uint8_t channel = 0;
 
-	DDRB |= (1 << PB0);
-
-	uint8_t mask = 0b00000000;
-
 	while(1){
 		uint16_t photoValue = ADC_read(channel);
 		if (photoValue>600){
-			PORTB = (1 << PB1);
+			PORTB = (1 << SERVO);
 			_delay_ms(2);
 			PORTB=0b00000000;
 			_delay_ms(1000);
 		}
 		else{
-			PORTB = (1 << PB1);
+			PORTB = (1 << SERVO);
 			_delay_ms(1);
 			PORTB=0b00000000;
 			_delay_ms(1000);
